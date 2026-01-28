eda466240037f0c2993f73cd1d004c4981aa9ccc
Rohit Satralkar <rohitsatralkar@gmail.com>
2026-01-29 00:28:30 +0100

    another try

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
     ['allure-playwright', {
       outputFolder: process.env.ALLURE_RESULTS || 'allure-results'
     }],
-    ['blob', { outputFolder: 'blob-report' }] 
+    ['blob'] 
   ],
   globalSetup: "./src/utils/global-setup.ts",
   use: {
