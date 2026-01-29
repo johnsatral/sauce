48f0a0a751289181609466f40571e9e50c148909
Rohit Satralkar <rohit.satralkar@rabobank.nl>
2026-01-29 09:58:28 +0100

    another morning attempt

--- playwright.config.ts
+++ playwright.config.ts
@@ -61,8 +61,13 @@ export default defineConfig({
         name: "fakeStoreAPI",
         testMatch: "**/specs/api/fakestore.spec.ts",
         use: {
-            ignoreHTTPSErrors: true
-        }
+            ignoreHTTPSErrors: true,
+            extraHTTPHeaders: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept': 'application/json',
+            'Accept-Language': 'en-US,en;q=0.9'
+        } 
+        },
     }
   ],
 });
